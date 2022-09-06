a6120130edbf4fc05b837f60973f11c142fdcfb1
Indra Pranata <indrapranata120@gmail.com>
2022-09-06 21:52:35 +0800

    Create sign-in api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const signup = require('../controllers/signup')
+const signin = require('../controllers/signin')
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 module.exports = router
