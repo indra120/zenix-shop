9d266125606eebb2c1cfaadd977be9ac502a0846
Indra Pranata <indrapranata120@gmail.com>
2022-09-07 10:30:14 +0800

    Create remove user API

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
-const verifyToken = require('../middlewares/verifyToken')
+const authorization = require('../middlewares/authorization')
 const editUser = require('../controllers/editUser')
+const removeUser = require('../controllers/removeUser')
 
-router.put('/:id', verifyToken, editUser)
+router.put('/:id', authorization, editUser) // Edit
+router.delete('/:id', authorization, removeUser) // Remove
 
 module.exports = router
