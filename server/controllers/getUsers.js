5b5141ef7eba3a6caa19a12e72ccfecb6ccda54d
Indra Pranata <indrapranata120@gmail.com>
2022-09-07 10:40:58 +0800

    Create get user API

---
+++ server/controllers/getUsers.js
@@ -0,0 +1,16 @@
+const User = require('../models/User')
+
+async function getUsers(req, res) {
+  const query = req.query.new
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find()
+
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json(err)
+  }
+}
+
+module.exports = getUsers
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
 const authorization = require('../middlewares/authorization')
+const admin = require('../middlewares/admin')
 const editUser = require('../controllers/editUser')
 const removeUser = require('../controllers/removeUser')
+const getUser = require('../controllers/getUser')
+const getUsers = require('../controllers/getUsers')
 
 router.put('/:id', authorization, editUser) // Edit
 router.delete('/:id', authorization, removeUser) // Remove
+router.get('/find/:id', admin, getUser) // Get user
+router.get('/', admin, getUsers) // Get users
 
 module.exports = router
