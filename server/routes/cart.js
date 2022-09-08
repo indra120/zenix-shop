9f152abf08b110969b61a955cd505e62e2234dd1
Indra Pranata <indrapranata120@gmail.com>
2022-09-08 10:40:58 +0800

    Create API to update the shopping cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,7 +3,9 @@ const verifyToken = require('../middlewares/verifyToken')
 const admin = require('../middlewares/admin')
 const authorization = require('../middlewares/authorization')
 const addToCart = require('../controllers/cart/addToCart')
+const updateCart = require('../controllers/cart/updateCart')
 
 router.post('/', verifyToken, addToCart)
+router.put('/:id', authorization, updateCart)
 
 module.exports = router
