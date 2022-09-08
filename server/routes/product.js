57074f214709b6b8e158ec1f3ac97f8b088bdaea
Indra Pranata <indrapranata120@gmail.com>
2022-09-08 10:06:06 +0800

    Create API to edit existing product

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const admin = require('../middlewares/admin')
 const create = require('../controllers/product/create')
+const update = require('../controllers/product/update')
 
 router.post('/', admin, create)
+router.put('/:id', admin, update)
 
 module.exports = router
