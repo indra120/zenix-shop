96026ce2b1a97756317488a131a33f40802b5f06
Indra Pranata <indrapranata120@gmail.com>
2022-09-08 10:22:18 +0800

    Create API to get all products

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,10 +4,12 @@ const createProduct = require('../controllers/product/createProduct')
 const updateProduct = require('../controllers/product/updateProduct')
 const deleteProduct = require('../controllers/product/deleteProduct')
 const getProduct = require('../controllers/product/getProduct')
+const getProducts = require('../controllers/product/getProducts')
 
 router.post('/', admin, createProduct)
 router.put('/:id', admin, updateProduct)
 router.delete('/:id', admin, deleteProduct)
 router.get('/find/:id', getProduct)
+router.get('/', getProducts)
 
 module.exports = router
