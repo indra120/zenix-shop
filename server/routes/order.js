187d897e132feb09040478f6a211cc88c021ba61
Indra Pranata <indrapranata120@gmail.com>
2022-09-09 11:15:20 +0800

    Create API to update an order

--- server/routes/order.js
+++ server/routes/order.js
@@ -3,7 +3,9 @@ const verifyToken = require('../middlewares/verifyToken')
 const admin = require('../middlewares/admin')
 const authorization = require('../middlewares/authorization')
 const createOrder = require('../controllers/order/createOrder')
+const updateOrder = require('../controllers/order/updateOrder')
 
 router.post('/', verifyToken, createOrder)
+router.put('/:id', admin, updateOrder)
 
 module.exports = router
