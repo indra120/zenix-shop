79231c232afae0c93cfd9f76ecf53d9518df13fd
Indra Pranata <indrapranata120@gmail.com>
2022-09-09 11:22:06 +0800

    Create API to get user orders

--- server/routes/order.js
+++ server/routes/order.js
@@ -5,9 +5,11 @@ const authorization = require('../middlewares/authorization')
 const createOrder = require('../controllers/order/createOrder')
 const updateOrder = require('../controllers/order/updateOrder')
 const deleteOrder = require('../controllers/order/deleteOrder')
+const getUserOrder = require('../controllers/order/getUserOrder')
 
 router.post('/', verifyToken, createOrder)
 router.put('/:id', admin, updateOrder)
 router.delete('/:id', admin, deleteOrder)
+router.get('/find/:userIdid', authorization, getUserOrder)
 
 module.exports = router
