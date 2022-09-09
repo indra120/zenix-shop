4290a214438013d7c39aa9e9d950b7188a56e820
Indra Pranata <indrapranata120@gmail.com>
2022-09-09 11:29:42 +0800

    Create API to get monthly income

--- server/routes/order.js
+++ server/routes/order.js
@@ -7,11 +7,13 @@ const updateOrder = require('../controllers/order/updateOrder')
 const deleteOrder = require('../controllers/order/deleteOrder')
 const getUserOrder = require('../controllers/order/getUserOrder')
 const getAllOrder = require('../controllers/order/getAllOrder')
+const getIncome = require('../controllers/order/getIncome')
 
 router.post('/', verifyToken, createOrder)
 router.put('/:id', admin, updateOrder)
 router.delete('/:id', admin, deleteOrder)
 router.get('/find/:userIdid', authorization, getUserOrder)
 router.get('/', admin, getAllOrder)
+router.get('/income', admin, getIncome)
 
 module.exports = router
