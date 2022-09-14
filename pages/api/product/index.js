c606cc98d5461144a7ce2b53f8e894207618c10b
Indra Pranata <indrapranata120@gmail.com>
2022-09-14 09:58:58 +0800

    remaking api to add & vew all products

---
+++ pages/api/product/index.js
@@ -0,0 +1,44 @@
+import Product from '../../../src/models/Product'
+import dbConnect from '../../../src/lib/dbConnect'
+import admin from '../../../src/middlewares/admin'
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case 'GET':
+      await dbConnect()
+
+      const { new: recent = undefined, category = undefined } = req.query
+
+      try {
+        let products
+
+        if (recent) {
+          products = await Product.find().sort({ createdAt: -1 }).limit(1)
+        } else if (category) {
+          products = await Product.find({
+            categories: {
+              $in: [category],
+            },
+          })
+        } else {
+          products = await Product.find()
+        }
+
+        res.status(200).json(products)
+      } catch (error) {
+        res.status(500).json(error)
+      }
+      break
+    case 'POST':
+      admin(req, res, async () => {
+        await dbConnect()
+
+        try {
+          const product = await new Product(req.body).save()
+          res.status(201).json(product)
+        } catch (error) {
+          res.status(500).json(error)
+        }
+      })
+  }
+}
